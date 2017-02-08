c6fe92a97b652ae9c7aeeab70f9cdc67a1ab7e7f
yamashita <hirotsugu_yamashita@dwango.co.jp>
2017-02-08 13:36:49 +0900

    unify showMessage method

--- src/showTemplateMessage.ts
+++ src/showTemplateMessage.ts
@@ -1,6 +1,5 @@
 import { InitParameterObject } from "./InitParameterObject";
 import { TemplateConfig } from "./TemplateConfig";
-import * as commons from "@akashic/akashic-cli-commons";
 
 /**
  * template.json の guideMessage 要素を出力する
@@ -12,15 +11,9 @@ export function showTemplateMessage(templateConfig: TemplateConfig, param: InitP
 function showTemplateGuideMessage(templateConfig: TemplateConfig, param: InitParameterObject): Promise<void> {
 	const guideMessage = templateConfig.guideMessage;
 	if (!guideMessage) return Promise.resolve();
-	return showMessage(guideMessage, param.logger);
-}
-
-function showMessage(message: string, logger: commons.Logger): Promise<void> {
-	return new Promise<void>((resolve, reject) => {
-		const texts = message.split("\n");
-		texts.forEach((text: string) => {
-			logger.print(text);
-		});
-		resolve();
+	const texts = guideMessage.split("\n");
+	texts.forEach((text: string) => {
+		param.logger.print(text);
 	});
+	Promise.resolve();
 }
